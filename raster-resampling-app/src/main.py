c5b38796a0d14d91f3be3878c43022da0a47ae85
bvagib <bvagib@gmail.com>
2025-12-23 11:04:59 +0100

    Implement dynamic configuration via environment variables

--- raster-resampling-app/src/main.py
+++ raster-resampling-app/src/main.py
@@ -9,18 +9,23 @@ def main():
     start = time.time()
     logging.info("Starting resampling...")
 
-    input_path='/data/input/input.tif'
-    output_path='/data/output/resampled.tif'
+    input_filename = os.getenv('INPUT_FILENAME', 'input.tif')
+    output_filename = os.getenv('OUTPUT_FILENAME', 'resampled.tif')
+    target_res = float(os.getenv('TARGET_RES', '100'))
+    resample_alg = os.getenv('RESAMPLE_ALG', 'near')
+
+    input_path = os.path.join('/data/input', input_filename)
+    output_path = os.path.join('/data/output', output_filename)
+    logging.info(f'Applied settings: Res={target_res}, Alg={resample_alg}')
 
     try:
-        gdal.Warp(output_path, input_path, xRes=100, yRes=100, resampleAlg='near')
-        logging.info("Kész")
+        gdal.Warp(output_path, input_path, xRes=target_res, yRes=target_res, resampleAlg=resample_alg)
     except Exception as e:
-        logging.error(f"Hiba: {e}")
+        logging.error(f"Error: {e}")
 
     end = time.time()
     elapsed = end - start
-    logging.info(f'Resampling finished in {elapsed:.2f} seconds')
+    logging.info(f'Resampling finished in {elapsed:.2f} seconds.')
 
 if __name__ == "__main__":
     main()
