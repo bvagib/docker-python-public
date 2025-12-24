5408ed24a676531e0ed00e13fe3d393ca45273d8
bvagib <bvagib@gmail.com>
2025-12-24 18:29:53 +0100

    Implement safe input detection & dynamic output naming

--- raster-resampling-app/src/main.py
+++ raster-resampling-app/src/main.py
@@ -2,21 +2,47 @@ import os
 import time
 import logging
 from osgeo import gdal
+import glob
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s] [%(name)s] %(message)s', level=logging.INFO)
 
 def main():
     start = time.time()
-    logging.info("Starting resampling...")
 
-    input_filename = os.getenv('INPUT_FILENAME', 'input.tif')
-    output_filename = os.getenv('OUTPUT_FILENAME', 'resampled.tif')
+    env_input = os.getenv('INPUT_FILENAME')
+
+    if env_input:
+        logging.info(f"Input file: {env_input}")
+        env_input_path = os.path.join('/data/input', env_input)
+        if os.path.exists(env_input_path):
+            input_path = env_input_path
+        else:
+            logging.error(f'No file named "{env_input}" found.')
+            return
+    else:       
+        found_files = glob.glob('/data/input/*.tif')
+        if found_files: 
+            count = len(found_files)
+            input_path = found_files[0]
+            input_name = os.path.basename(input_path)
+            if count > 1:
+                logging.warning(f"Found {count} files. Processing the first one: {input_name}.")
+            else:
+                logging.info(f"Found input file: {input_name}.")
+        else:
+            logging.error("No files found.")
+            return
+
+    filename = os.path.basename(input_path)
+    filename_without_extension = os.path.splitext(filename)[0]
+    new_filename = f"{filename_without_extension}_resampled.tif"
+    output_path = os.path.join('/data/output', new_filename)
+
     target_res = float(os.getenv('TARGET_RES', '100'))
     resample_alg = os.getenv('RESAMPLE_ALG', 'near')
 
-    input_path = os.path.join('/data/input', input_filename)
-    output_path = os.path.join('/data/output', output_filename)
     logging.info(f'Applied settings: Res={target_res}, Alg={resample_alg}')
+    logging.info("Starting resampling...")
 
     try:
         gdal.Warp(output_path, input_path, xRes=target_res, yRes=target_res, resampleAlg=resample_alg)
